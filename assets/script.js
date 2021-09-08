6680c479f10ec2febb7760911ac48cc60b0df46c
Zmoore <zmoore371@gmail.com>
2021-09-07 23:17:08 -0400

    New functions to display text in html for current day. Started a function to fetch the 5 day forecast but have not completed it. Function display current will eventually take the data from the getApiToday function and display it on the webpage. Will need to find a way to loop through 5 day forecast so I dont have to repeat a bunch of code. Not sure if current class names for the weather cards will work or if i should change them to the same name and loop through it that way.

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,68 @@
 var APIkey = "87fda1a82cfddd0be50e7d0dba921aff";
-var search = $(".searchBtn");
-var city = $(".city-name");
+var searchEl = $(".searchBtn");
+var cityEl = $(".city-name");
 
 
 
 
-search.on("click", function(event) {
+searchEl.on("click", function(event) {
     event.preventDefault();
+    console.log(cityEl.val());
     
-    console.log(city);
-    console.log('abd');
+    var city = cityEl.val()
+    
+    if(city) {
+        getApiToday(city)
+        cityEl.val('');
+        $(".weather-info").show();
+        $(".forecast").show();
+
+    } else {
+        alert("yo you gotta put some text in this mug")
+    }
+
+    
 })
 
-function getApi () {
-    var APIkey = "87fda1a82cfddd0be50e7d0dba921aff";
-    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=" + APIkey
+function getApiToday (city) {
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=" + APIkey
     console.log(requestUrl)
     
     fetch(requestUrl)
         .then(function (response){
             return response.json();
         })
+    //Put an if 404 alert thing somewhere in here probably
+
+}
+
+function getApiForecast (city) {
+    var requestUrl = "whatever the 5 day forecast api call is" + city + "likely some more crap" + APIkey
+
+    // fetch(requestUrl) then blah blah blah
+}
+
+function displayCurrent() {
+    var currentCity = $(".current-city");
+    var currentTemp = $(".current-temp");
+    var currentWind = $(".current-wind");
+    var currentHumidity = $(".current-humidity");
+    console.log("hey")
+    
+    currentCity.text("Raleigh")
+    currentTemp.text("Current Temp: " + 70)
+    currentWind.text("Current Wind Speed: " + 15)
+    currentHumidity.text("Current Humidity: " + 60 + "%")
 
 
 }
+
+function init() {
+    $(".weather-info").hide()
+    $(".forecast").hide()
+}
+
+
+// init();
+
+displayCurrent();
