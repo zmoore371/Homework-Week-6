d3e0fff5e110dfdbee359561feaa8874e0cbfc8e
Zmoore <zmoore371@gmail.com>
2021-09-07 22:09:13 -0400

    Slight CSS changes for different colors no functionality changes. JS file now has function get API which has a request url for data for london weather. If you call the function it will return data successfully. Run with it from here

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,26 @@
-var APIkey = "87fda1a82cfddd0be50e7d0dba921aff"
+var APIkey = "87fda1a82cfddd0be50e7d0dba921aff";
+var search = $(".searchBtn");
+var city = $(".city-name");
+
+
+
+
+search.on("click", function(event) {
+    event.preventDefault();
+    
+    console.log(city);
+    console.log('abd');
+})
+
+function getApi () {
+    var APIkey = "87fda1a82cfddd0be50e7d0dba921aff";
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=" + APIkey
+    console.log(requestUrl)
+    
+    fetch(requestUrl)
+        .then(function (response){
+            return response.json();
+        })
+
+
+}
