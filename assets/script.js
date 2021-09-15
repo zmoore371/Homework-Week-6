81a4662b75b7f4ce2fa8d2c8324353c12bbb59f4
Zmoore <zmoore371@gmail.com>
2021-09-14 21:06:31 -0400

    javascript formatting. Added instructions to clear search history in html. Styled instructions with CSS

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var searchEl = $("#search");
 var cities = $("#cities");
 var cityEl = $(".city-name");
 var previousSearches = [];
-var renderedButtons = $(".city-info")
+var renderedButtons = $(".city-info");
 
 searchEl.on("click", function (event) {
     event.preventDefault();
@@ -25,11 +25,11 @@ searchEl.on("click", function (event) {
 
 cities.on("click", function (event) {
     if (event.target.matches('.history')) {
-        event.preventDefault()
+        event.preventDefault();
         var searchTerm = event.target.getAttribute('data-city');
-        getPastApiToday(searchTerm)
-        getApiForecast(searchTerm)
-        cityEl.val('')
+        getPastApiToday(searchTerm);
+        getApiForecast(searchTerm);
+        cityEl.val('');
     }
 })
 
@@ -44,7 +44,7 @@ function getApiToday(city) {
                     getUv(data);
                     $(".weather-info").show();
                     $(".forecast").show();
-                    localStorage.getItem("history")
+                    localStorage.getItem("history");
                     searchText = city.charAt(0).toUpperCase() + city.slice(1);
                     previousSearches.push(searchText);
                     storePrevious(previousSearches);
@@ -74,7 +74,7 @@ function getPastApiToday(city) {
 };
 
 function getApiForecast(city) {
-    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIkey
+    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIkey;
 
     fetch(requestUrl)
         .then(function (response) {
@@ -86,7 +86,7 @@ function getApiForecast(city) {
                 return;
             }
         })
-}
+};
 
 function displayCurrent(today) {
     var currentCity = $(".current-city");
@@ -100,122 +100,117 @@ function displayCurrent(today) {
     currentWind.text("Current Wind Speed: " + today.wind.speed + "MPH");
     currentHumidity.text("Current Humidity: " + today.main.humidity + "%");
     currentImg[0].src = "http://openweathermap.org/img/wn/" + today.weather[0].icon + "@2x.png"
-}
+};
 
 function getUv(today) {
-    lat = today.coord.lat
-    lon = today.coord.lon
-    requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + APIkey
+    lat = today.coord.lat;
+    lon = today.coord.lon;
+    requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + APIkey;
 
     fetch(requestUrl)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    displayUv(data)
+                    displayUv(data);
                 })
             } else {
                 return;
             }
         })
-}
+};
 
 function displayUv(todayUv) {
-    currentUv = $(".uv-index")
-    uvIndex = todayUv.current.uvi
-    currentUv.text(uvIndex + " UV Index")
+    currentUv = $(".uv-index");
+    uvIndex = todayUv.current.uvi;
+    currentUv.text(uvIndex + " UV Index");
 
     if (uvIndex < 2) {
-        currentUv.removeClass("moderate")
-        currentUv.removeClass("high")
-        currentUv.addClass("favorable")
+        currentUv.removeClass("moderate");
+        currentUv.removeClass("high");
+        currentUv.addClass("favorable");
     } else if (uvIndex < 5) {
-        currentUv.removeClass("favorable")
-        currentUv.removeClass("high")
-        currentUv.addClass("moderate")
+        currentUv.removeClass("favorable");
+        currentUv.removeClass("high");
+        currentUv.addClass("moderate");
     } else {
-        currentUv.removeClass("moderate")
-        currentUv.removeClass("favorable")
-        currentUv.addClass("high")
+        currentUv.removeClass("moderate");
+        currentUv.removeClass("favorable");
+        currentUv.addClass("high");
     }
 
-}
-
+};
 
 function displayForecast(forecast) {
     x = 1
-    nextDay = moment().add(x, "days").format("YYYY-MM-DD")
-    limitedForecast = []
-    forecastDay = $(".forecast-date")
-    forecastImg = $(".forecast-img")
-    forecastTemp = $(".forecast-temp")
-    forecastWind = $(".forecast-wind")
-    forecastHumidity = $(".forecast-humidity")
+    nextDay = moment().add(x, "days").format("YYYY-MM-DD");
+    limitedForecast = [];
+    forecastDay = $(".forecast-date");
+    forecastImg = $(".forecast-img");
+    forecastTemp = $(".forecast-temp");
+    forecastWind = $(".forecast-wind");
+    forecastHumidity = $(".forecast-humidity");
 
     for (i = 0; i < forecast.list.length; i++) {
         if (forecast.list[i].dt_txt === nextDay + " 12:00:00") {
             x = x + 1
             limitedForecast.push(forecast.list[i]);
-            nextDay = moment().add((x), "days").format("YYYY-MM-DD")
+            nextDay = moment().add((x), "days").format("YYYY-MM-DD");
         }
     }
-    console.log(forecast)
-
     // before putting this here if you looked at the weather too early it would not show a 5th day due to the 12:00 forecast being avaliable yet. This check makes sure that the array has 5 inputs to push to screen by appending the last avaliable forecast slot to limitedForecast
     if (limitedForecast.length !== 5) {
-        limitedForecast.push(forecast.list[39])
-        console.log(limitedForecast)
+        limitedForecast.push(forecast.list[39]);
+        console.log(limitedForecast);
     }
 
     for (i = 0; i < limitedForecast.length; i++) {
         if (limitedForecast === null) {
             return;
         } else {
-            forecastDay[i].innerHTML = limitedForecast[i].dt_txt.substring(5, 10)
-            forecastTemp[i].innerHTML = (Math.round(limitedForecast[i].main.temp)) + "°F"
-            forecastWind[i].innerHTML = "Wind: " + limitedForecast[i].wind.speed + " MPH"
-            forecastHumidity[i].innerHTML = "Humidity: " + limitedForecast[i].main.humidity + "%"
-            forecastImg[i].src = "https://openweathermap.org/img/wn/" + limitedForecast[i].weather[0].icon + "@2x.png"
+            forecastDay[i].innerHTML = limitedForecast[i].dt_txt.substring(5, 10);
+            forecastTemp[i].innerHTML = (Math.round(limitedForecast[i].main.temp)) + "°F";
+            forecastWind[i].innerHTML = "Wind: " + limitedForecast[i].wind.speed + " MPH";
+            forecastHumidity[i].innerHTML = "Humidity: " + limitedForecast[i].main.humidity + "%";
+            forecastImg[i].src = "https://openweathermap.org/img/wn/" + limitedForecast[i].weather[0].icon + "@2x.png";
         }
     }
-}
+};
 
 function renderPrevious() {
-
     for (var i = 0; i < searches.length; i++) {
         var btn = document.createElement("button");
-        btn.textContent = searches[i]
-        btn.setAttribute("class", "history")
-        btn.setAttribute("data-city", searches[i])
-        cities.append(btn)
+        btn.textContent = searches[i];
+        btn.setAttribute("class", "history");
+        btn.setAttribute("data-city", searches[i]);
+        cities.append(btn);
     }
-}
+};
 
 function storePrevious(previousSearches, searches) {
     localStorage.setItem("history", JSON.stringify(previousSearches))
 
     if (previousSearches !== null) {
         var btn = document.createElement("button");
-        btn.textContent = previousSearches.at(-1)
-        btn.setAttribute("class", "history")
-        btn.setAttribute("data-city", previousSearches.at(-1))
-        cities.append(btn)
+        btn.textContent = previousSearches.at(-1);
+        btn.setAttribute("class", "history");
+        btn.setAttribute("data-city", previousSearches.at(-1));
+        cities.append(btn);
     }
-}
+};
 
 function init() {
-    $(".weather-info").hide()
-    $(".forecast").hide()
-    $(".forecast-header").hide()
+    $(".weather-info").hide();
+    $(".forecast").hide();
+    $(".forecast-header").hide();
 
     searches = JSON.parse(localStorage.getItem("history"));
     if (searches === null) {
         return;
     } else {
-        previousSearches = searches
+        previousSearches = searches;
     }
-
     renderPrevious();
-}
+};
 
 init();
 
